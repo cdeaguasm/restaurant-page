89d508aa97506429d34c5a395d077820a7689b89
Carlos De Aguas <cdeaguasm@gmail.com>
2019-04-05 21:54:08 -0500

    Add styles to menu and banner

---
+++ js/menu.js
@@ -0,0 +1,7 @@
+let iconMenu = document.getElementById('icon-menu');
+let menu = document.getElementById('menu');
+
+iconMenu.addEventListener('click', function(){
+    'use strict';
+    menu.classList.toggle('mostrar-menu');
+});
